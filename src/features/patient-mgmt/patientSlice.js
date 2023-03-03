e91e10c8253a9f810065916c93fc49233e8e0033
virsavik <dhuuloc8818@gmail.com>
2023-03-03 20:34:55 +0700

    Make patient management UI

--- src/features/patient-mgmt/patientSlice.js
+++ src/features/patient-mgmt/patientSlice.js
@@ -1,44 +1,87 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const apiURL = process.env.REACT_APP_API_URL;
+const apiURL = `${process.env.REACT_APP_API_URL}/Patient`;
 
 const initialState = {
-  patients: [],
-  selectedPatient: null,
+  entity: null,
+  entities: [],
   loading: false,
   error: null,
 };
 
-// Actions
-
-export const fetchPatients = createAsyncThunk('patient/fetchPatients', async () => {
-  const resp = await axios.get(`${apiURL}/patients`, {
+/**
+ * Fetch all patients
+ * @param {object} criteria - search criteria
+ * @param {Array} criteria.filters - array of filters ```{fieldName: "name", operation: "like", value: "John"}```
+ * @param {object} criteria.sortBy - sort criteria ```{fieldName: "name", accending: true}```
+ * @param {number} criteria.pageIndex - page index number
+ * @param {number} criteria.pageSize - page size number
+ */
+export const fetchAll = createAsyncThunk('patient/fetchAll', async (criteria) => {
+  const resp = await axios.post(`${apiURL}/search`, criteria, {
     headers: {
       // Authorization: `Bearer ${localStorage.getItem("token")}`,
       'Content-Type': 'application/json',
     },
   });
+
   return resp.data;
 });
 
-export const getPatient = createAsyncThunk('patient/getPatient', async (id) => {
-  const resp = await axios.get(`${apiURL}/patients/${id}`, {
+/**
+ * Create a patient
+ */
+export const createOne = createAsyncThunk('patient/createOne', async (values) => {
+  const resp = await axios.post(`${apiURL}`, values, {
     headers: {
       // Authorization: `Bearer ${localStorage.getItem("token")}`,
       'Content-Type': 'application/json',
     },
   });
+
   return resp.data;
 });
 
-export const deletePatient = createAsyncThunk('patient/deletePatient', async (id) => {
-  const resp = await axios.delete(`${apiURL}/patients/${id}`, {
+/**
+ * Fetch a patient
+ */
+export const fetchOne = createAsyncThunk('patient/fetchOne', async (id) => {
+  const resp = await axios.get(`${apiURL}/${id}`, {
     headers: {
       // Authorization: `Bearer ${localStorage.getItem("token")}`,
       'Content-Type': 'application/json',
     },
   });
+
+  return resp.data;
+});
+
+/**
+ * Update a patient
+ */
+export const updateOne = createAsyncThunk('patient/updateOne', async (values) => {
+  const resp = await axios.put(`${apiURL}/${values.id}`, values, {
+    headers: {
+      // Authorization: `Bearer ${localStorage.getItem("token")}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return resp.data;
+});
+
+/**
+ * Delete a patient
+ */
+export const deleteOne = createAsyncThunk('patient/deleteOne', async (id) => {
+  const resp = await axios.delete(`${apiURL}/${id}`, {
+    headers: {
+      // Authorization: `Bearer ${localStorage.getItem("token")}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
   return resp.data;
 });
 
@@ -47,38 +90,95 @@ const patientSlice = createSlice({
   initialState,
   extraReducers(builder) {
     builder
-      .addCase(fetchPatients.pending, (state) => {
+      .addCase(fetchAll.pending, (state) => {
         state.loading = true;
         state.error = null;
       })
-      .addCase(fetchPatients.fulfilled, (state, action) => {
+      .addCase(fetchAll.fulfilled, (state, action) => {
+        const { data, isSuccess, message } = action.payload;
+
         state.loading = false;
-        state.patients = action.payload;
+        if (!isSuccess) {
+          state.error = { message };
+        } else {
+          state.entities = data.data;
+        }
       })
-      .addCase(fetchPatients.rejected, (state, action) => {
+      .addCase(fetchAll.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error;
       })
-      .addCase(getPatient.pending, (state) => {
+      .addCase(createOne.pending, (state) => {
         state.loading = true;
         state.error = null;
       })
-      .addCase(getPatient.fulfilled, (state, action) => {
+      .addCase(createOne.fulfilled, (state, action) => {
+        const { data, isSuccess, message } = action.payload;
+
         state.loading = false;
-        state.selectedPatient = action.payload;
+        if (!isSuccess) {
+          state.error = { message };
+        } else {
+          state.entities.push(data);
+        }
       })
-      .addCase(getPatient.rejected, (state, action) => {
+      .addCase(createOne.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error;
       })
-      .addCase(deletePatient.pending, (state) => {
+      .addCase(fetchOne.pending, (state) => {
         state.loading = true;
         state.error = null;
       })
-      .addCase(deletePatient.fulfilled, (state) => {
+      .addCase(fetchOne.fulfilled, (state, action) => {
+        const { data, isSuccess, message } = action.payload;
+
         state.loading = false;
+        if (!isSuccess) {
+          state.error = { message };
+        } else {
+          state.entity = data;
+        }
       })
-      .addCase(deletePatient.rejected, (state, action) => {
+      .addCase(fetchOne.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error;
+      })
+      .addCase(updateOne.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateOne.fulfilled, (state, action) => {
+        const { data, isSuccess, message } = action.payload;
+
+        state.loading = false;
+        if (!isSuccess) {
+          state.error = { message };
+        } else {
+          const idx = state.entities.findIndex((entity) => entity.id === data.id);
+          state.entities[idx] = data;
+        }
+      })
+      .addCase(updateOne.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error;
+      })
+      .addCase(deleteOne.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteOne.fulfilled, (state, action) => {
+        const { data, isSuccess, message } = action.payload;
+        const { arg } = action.meta;
+
+        state.loading = false;
+        if (!isSuccess) {
+          state.error = { message };
+        } else {
+          state.entities.filter((entity) => entity.id !== arg);
+        }
+      })
+      .addCase(deleteOne.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error;
       });
